b177c73de5eea9af816bc3f63f39314aa000b7d0
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2026-02-09 00:59:12 +0530

    fix: Use ESM import for tailwind typography plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
     content: [
         "./index.html",
@@ -48,6 +50,6 @@ export default {
         },
     },
     plugins: [
-        require('@tailwindcss/typography'),
+        typography,
     ],
 }
